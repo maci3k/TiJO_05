122fb4829ea6845aaa448c0ba30a0d8d81730e3f
Maciej Klimczyk <jackson93.07@gmail.com>
2016-03-23 12:42:57 +0100

    First part of exercise

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -4,13 +4,29 @@ describe('app', function () {
     var app = window.app;
 
     describe('generateMessage', function () {
-
+        it('should return number of vowels in string and whether string is a palindrome', function() {
+            expect(app.generateMessage('kajak')).toEqual({vowel: 2, palindrome: true});
+            expect(app.generateMessage('mam')).toEqual({vowel: 1, palindrome:true});
+            expect(app.generateMessage('ala ma kota')).toEqual({vowel: 5, palindrome: false});
+        });
+        //asdasjdhasjkdhasjkdh
+        xit('should raise excecption when string is empty', function() {
+            expect(app.generateMessage('')).toThrow(new Error('Empty string!'));
+        });
     });
 
     describe('isPalindrome', function () {
 
         describe('toHaveBeenCalled', function () {
-
+            beforeAll(function() {
+                spyOn(app, 'isPalindrome');
+                app.isPalindrome('kajak');
+            });
+            it('should call isPalindrome function', function() {
+                expect(app.isPalindrome).toHaveBeenCalled();
+                expect(app.isPalindrome).toHaveBeenCalledWith('kajak');
+                expect(app.isPalindrome).not.toHaveBeenCalledWith('ala');
+            });
         });
 
         describe('and.callThrough', function () {
